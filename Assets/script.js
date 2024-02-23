2d6ea83ef074a58ad23a1832374acb791110c13e
Bijay-Gurung <stha4580@gmail.com>
2024-02-23 20:45:15 +0545

    Adding new functionality

--- Assets/script.js
+++ Assets/script.js
@@ -3,19 +3,6 @@ function myGoals(){
     alert("Here, I am go to set my Goals");
 }
 
-// To change Text
-let text = document.getElementById("changeText");
-let textList = ["Good Morning","Welcome Back"];
-let i = 0;
-
-function changingText(){
-    text.textContent = textList[i];
-    i= (i+1)%textList.length;
-}
-
-changingText();
-setInterval(changingText,2000);
-
 // To get Day, Date and Time
 //for Day
 let day = document.getElementById("day");
@@ -70,6 +57,36 @@ function displayTime(){
         hrs = 12;
     }
     time.innerHTML = hrs+':'+min+':'+sec+' '+session.innerHTML;
-}
-setInterval(displayTime,10);
 
+    // To change Text
+    let text1 = document.getElementById("change");
+    if(hrs<=5 && hrs<=11 && session.innerHTML === "Am"){
+        text1.innerHTML = "Good Morning";
+    }
+    else if(hrs>=0 && hrs<=3 && session.innerHTML === "Pm"){
+        text1.innerHTML = "Good Afternoon";
+    }
+    else if(hrs<=4 && hrs<=7 &&session.innerHTML === "Pm"){
+        text1.innerHTML = "Good Evening";
+    }
+    else if(hrs>=8 && session.innerHTML === "Pm"){
+        text1.innerHTML = "Good Night";
+    }
+}
+setInterval(displayTime,2000);
+let text1 = document.getElementById("change");
+let text2 = document.getElementById("changeText");
+let textList1 = ["Welcome Back","Let's Start"];
+let textList2 = ["Take a Rest","Play Some Games","Go to sleep"];
+let i = 0;
+function changingText(){
+    if(text1.innerHTML === "Good Night"){
+        text2.textContent = textList2[i];
+        i = (i+1)%textList2.length;
+    }else{
+        text2.textContent = textList1[i];
+        i= (i+1)%textList1.length;
+    }
+}
+changingText();
+setInterval(changingText,3000);
